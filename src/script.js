a823178dbd91b7f113a6c9be8798d61cc66d3c07
litnomad <128262375+litnomad@users.noreply.github.com>
2026-06-25 13:04:38 -0400

    add script and test

--- src/script.js
+++ src/script.js
@@ -6,25 +6,53 @@ class Node {
   }
 }
 
-class Tree {
-  constructor(array) {
-    this.root = this.#buildTree(array);
+class Queue {
+  constructor() {
+    this.item = {};
+    this.headIndex = 0;
+    this.tailIndex = 0;
   }
 
-  #buildTree(array, start = 0, end = array.length - 1) {
-    if (start > end) {
-      return null;
-    }
+  enqueue(item) {
+    this.item[this.headIndex] = item;
+    this.headIndex++;
+  }
 
-    // sorts and removes duplicate items
-    array
+  dequeue() {
+    const value = this.item[this.tailIndex];
+    delete this.item[this.tailIndex];
+    this.tailIndex++;
+    return value;
+  }
+
+  isEmpty() {
+    return this.headIndex === this.tailIndex;
+  }
+}
+
+class Tree {
+  constructor(array) {
+    this.root = this.#buildTree(this.#sort(array));
+  }
+
+  // removes duplicate items from the array and returns it sorted
+  #sort(array) {
+    return array
       .reduce((accum, current) => {
         if (accum.includes(current) === false) {
           accum.push(current);
         }
         return accum;
       }, [])
-      .sort();
+      .sort((a, b) => {
+        return a - b;
+      });
+  }
+
+  #buildTree(array, start = 0, end = array.length - 1) {
+    if (start > end) {
+      return null;
+    }
 
     const middle = Math.ceil((start + end) / 2);
     const root = new Node(array[middle]);
@@ -34,9 +62,375 @@ class Tree {
 
     return root;
   }
+
+  levelOrderForEach(
+    root = this.root,
+    result = [],
+    nextLeft = [],
+    nextRight = [],
+  ) {
+    if (root === undefined) {
+      throw new Error("Tree node needs to be provided.");
+    }
+
+    if (root === null) {
+      return;
+    }
+
+    if (
+      nextLeft.length > 0 &&
+      nextLeft.every((node) => node == null) &&
+      nextRight.length > 0 &&
+      nextRight.every((node) => node == null)
+    ) {
+      return result;
+    }
+
+    const q = new Queue();
+
+    if (result.length === 0) {
+      q.enqueue(root.data);
+    }
+
+    if (nextLeft.length === 0 && nextRight.length === 0) {
+      if (root.left) {
+        q.enqueue(root.left.data);
+      }
+      if (root.right) {
+        q.enqueue(root.right.data);
+      }
+    }
+
+    nextLeft.forEach((node) => {
+      if (node !== null) {
+        if (node.left) {
+          q.enqueue(node.left.data);
+        }
+        if (node.right) {
+          q.enqueue(node.right.data);
+        }
+      }
+    });
+
+    nextRight.forEach((node) => {
+      if (node !== null) {
+        if (node.left) {
+          q.enqueue(node.left.data);
+        }
+        if (node.right) {
+          q.enqueue(node.right.data);
+        }
+      }
+    });
+
+    while (!q.isEmpty()) {
+      const printData = q.dequeue();
+      result.push(printData);
+    }
+
+    if (nextRight.length === 0 && nextLeft.length === 0) {
+      nextLeft.push(root.left);
+      nextRight.push(root.right);
+    } else {
+      let copy = nextLeft;
+      let copy2 = nextRight;
+      nextLeft = [];
+      nextRight = [];
+
+      copy.forEach((node) => {
+        if (node !== null) {
+          nextLeft.push(node.left, node.right);
+        }
+      });
+      copy2.forEach((node) => {
+        if (node !== null) {
+          nextRight.push(node.left, node.right);
+        }
+      });
+    }
+
+    this.levelOrderForEach(root, result, nextLeft, nextRight);
+
+    return result;
+  }
+
+  preOrderForEach(root = this.root, result = []) {
+    if (root === undefined) {
+      throw new Error("Tree node needs to be provided.");
+    }
+
+    if (root === null) return;
+
+    const q = new Queue();
+    q.enqueue(root.data);
+
+    while (!q.isEmpty()) {
+      const printData = q.dequeue();
+      result.push(printData);
+    }
+
+    let left = root.left;
+    let right = root.right;
+    this.preOrderForEach(left, result);
+    this.preOrderForEach(right, result);
+
+    return result;
+  }
+
+  inOrderForEach(root = this.root, result = []) {
+    if (root === undefined) {
+      throw new Error("Tree node needs to be provided.");
+    }
+
+    if (root === null) return;
+
+    let left = root.left;
+    this.inOrderForEach(left, result);
+
+    const q = new Queue();
+    q.enqueue(root.data);
+
+    while (!q.isEmpty()) {
+      const printData = q.dequeue();
+      result.push(printData);
+    }
+
+    let right = root.right;
+    this.inOrderForEach(right, result);
+
+    return result;
+  }
+
+  postOrderForEach(root = this.root, result = []) {
+    if (root === undefined) {
+      throw new Error("Tree node needs to be provided.");
+    }
+
+    if (root === null) return;
+
+    let left = root.left;
+    this.postOrderForEach(left, result);
+    let right = root.right;
+    this.postOrderForEach(right, result);
+
+    const q = new Queue();
+    q.enqueue(root.data);
+
+    while (!q.isEmpty()) {
+      const printData = q.dequeue();
+      result.push(printData);
+    }
+
+    return result;
+  }
+
+  includes(value) {
+    const array = this.levelOrderForEach(this.root);
+    return array.includes(value);
+  }
+
+  insert(value, root = this.root) {
+    if (value < root.data) {
+      if (root.left === null) {
+        root.left = new Node(value);
+      } else {
+        this.insert(root.left, value);
+      }
+    } else {
+      if (root.right === null) {
+        root.right = new Node(value);
+      } else {
+        this.insert(value, root.right);
+      }
+    }
+
+    return root;
+  }
+
+  #successor(root) {
+    let current = root.right;
+    let pointer = "";
+
+    while (current !== null && current.left !== null) {
+      pointer = current;
+      current = current.left;
+    }
+
+    if (pointer == "") {
+      root.right = null;
+    } else {
+      pointer.left = null;
+    }
+    return current.data;
+  }
+
+  deleteItem(root, value, previous = "") {
+    if (root === null) {
+      return;
+    }
+
+    if (root.data === value) {
+      // children
+      if (root.left && root.right) {
+        root.data = this.#successor(root);
+        return root;
+      }
+
+      // no children
+      if (root.left === null && root.right === null) {
+        if (root === previous.left) {
+          previous.left = null;
+          return root;
+        }
+        if (root === previous.right) {
+          previous.right = null;
+          return root;
+        }
+      }
+      // 1 children
+      if (root.left === null) {
+        previous.left = root.right;
+        return root;
+      }
+      if (root.right === null) {
+        previous.left = root.left;
+        return root;
+      }
+    }
+
+    if (value < root.data) {
+      previous = root;
+      this.deleteItem(root.left, value, previous);
+    } else {
+      previous = root;
+      this.deleteItem(root.right, value, previous);
+    }
+
+    return root;
+  }
+
+  height(value, root = this.root) {
+    // return undefined if value is not in the tree
+    const tree = this.inOrderForEach(this.root);
+    const truthy = tree.includes(value);
+    if (!truthy) {
+      return;
+    }
+
+    let i = 0;
+
+    if (root === null) {
+      return i;
+    }
+
+    // if node value exists, count the number of edges from the node to the leaf node
+    let current = root;
+    if (current.data === value) {
+      let leftSide = current;
+      let rightSide = current;
+
+      while (leftSide.left !== null || rightSide.right !== null) {
+        if (leftSide.left !== null) {
+          leftSide = leftSide.left;
+        }
+        if (rightSide.right !== null) {
+          rightSide = rightSide.right;
+        }
+        i++;
+      }
+      return i;
+    }
+    // traverse the tree nodes
+    if (root.data !== value) {
+      i += this.height(value, current.left);
+      i += this.height(value, current.right);
+
+      // i must be returned to avoid NaN
+      return i;
+    } else {
+      null;
+    }
+  }
+
+  depth(value, current = this.root) {
+    const tree = this.levelOrderForEach(this.root);
+    const truthy = tree.includes(value);
+    if (!truthy) {
+      return;
+    }
+
+    let i = 0;
+
+    if (current.data === value) {
+      return i;
+    }
+
+    if (value > current.data) {
+      current = current.right;
+    } else {
+      current = current.left;
+    }
+    i++;
+
+    i += this.depth(value, current);
+
+    return i;
+  }
+
+  isBalanced(root = this.root) {
+    if (root === null) return;
+
+    // left subtree
+    let leftTree = root;
+    if (root.left != null) {
+      leftTree = root.left;
+    }
+
+    // right subtree
+    let rightTree = root;
+    if (root.right != null) {
+      rightTree = root.right;
+    }
+
+    // get the height of left subtree
+    const leftHeight = this.height(leftTree.data);
+
+    // get the height of right subtree
+    const rightHeight = this.height(rightTree.data);
+
+    // height difference between left and right subtree
+    if (Math.abs(leftHeight - rightHeight) <= 1) {
+      return true;
+    }
+    if (Math.abs(leftHeight - rightHeight) > 1) {
+      return false;
+    }
+  }
+
+  rebalance() {
+    const array = this.inOrderForEach(this.root);
+    this.root = this.#buildTree(this.#sort(array));
+  }
 }
 
-const test = new Tree([1, 2, 3, 4, 5]);
-console.log(test);
+const balance = new Tree([1, 2, 3, 4, 5]);
+console.log(balance.levelOrderForEach());
+console.log(balance.preOrderForEach());
+console.log(balance.postOrderForEach());
+console.log(balance.inOrderForEach());
+balance.insert(324);
+balance.insert(400);
+balance.insert(500);
+console.log(balance.isBalanced());
+balance.rebalance();
+console.log(balance.isBalanced());
+//console.log(balance.levelOrderForEach());
+console.log(balance.preOrderForEach());
+console.log(balance.postOrderForEach());
+console.log(balance.inOrderForEach());
+
+const empty = new Tree([]);
+console.log(empty);
+empty.levelOrderForEach();
 
 export { Tree };
--- src/script.test.js
+++ src/script.test.js
@@ -1,10 +1,10 @@
 import { Tree } from "./script.js";
 
-describe("Create Tree", () => {
-  const bst = new Tree([1, 2, 3, 4, 5]);
+describe("Initiate Tree", () => {
+  const tree = new Tree([1, 2, 3, 4, 5]);
 
-  test("Expecting it to be an object of nodes ordered by roots", () => {
-    expect(bst).toEqual({
+  test("Turn an array into a Binary Search Tree containing a root of subtrees", () => {
+    expect(tree).toEqual({
       root: {
         data: 3,
         left: {
@@ -26,6 +26,212 @@ describe("Create Tree", () => {
           right: null,
         },
       },
+    });
+  });
+
+  describe("Binary Tree Traversals", () => {
+    test("levelOrderForEach() should print data in breadth-first order", () => {
+      expect(tree.levelOrderForEach(tree.root)).toStrictEqual([3, 2, 5, 1, 4]);
+    });
+
+    test("preOrderForEach() should print data in depth-first level with each child as the root of each left subtree", () => {
+      expect(tree.preOrderForEach(tree.root)).toStrictEqual([3, 2, 1, 5, 4]);
+    });
+
+    test("inOrderForEach() should print data from the Tree in sorted order", () => {
+      expect(tree.inOrderForEach(tree.root)).toStrictEqual([1, 2, 3, 4, 5]);
+    });
+
+    test("postOrderForEach() should print data starting from the bottom of the leaf nodes back up to the root", () => {
+      expect(tree.postOrderForEach(tree.root)).toStrictEqual([1, 2, 4, 5, 3]);
+    });
+  });
+
+  describe("Methods", () => {
+    test("includes(value) should return true if the value is in the tree. If it isn't in the tree, then returns false", () => {
+      expect(tree.includes(2)).toBeTruthy();
+    });
+
+    test("insert(value) should insert a new node with that value into the tree", () => {
+      expect(tree.insert(6)).toEqual({
+        data: 3,
+        left: {
+          data: 2,
+          left: {
+            data: 1,
+            left: null,
+            right: null,
+          },
+          right: null,
+        },
+        right: {
+          data: 5,
+          left: {
+            data: 4,
+            left: null,
+            right: null,
+          },
+          right: {
+            data: 6,
+            left: null,
+            right: null,
+          },
+        },
+      });
+    });
+
+    test("deleteItem(value) accepts a value and removes it from the tree", () => {
+      expect(tree.deleteItem(tree.root, 3)).toEqual({
+        data: 4,
+        left: {
+          data: 2,
+          left: {
+            data: 1,
+            left: null,
+            right: null,
+          },
+          right: null,
+        },
+        right: {
+          data: 5,
+          left: null,
+          right: {
+            data: 6,
+            left: null,
+            right: null,
+          },
+        },
+      });
+    });
+
+    test("deleteItem(value) should still work after previous node removal", () => {
+      expect(tree.deleteItem(tree.root, 2)).toEqual({
+        data: 4,
+        left: {
+          data: 1,
+          left: null,
+          right: null,
+        },
+        right: {
+          data: 5,
+          left: null,
+          right: {
+            data: 6,
+            left: null,
+            right: null,
+          },
+        },
+      });
+    });
+
+    test("deleteItem(6) should remove the last leaf node", () => {
+      expect(tree.deleteItem(tree.root, 6)).toEqual({
+        data: 4,
+        left: {
+          data: 1,
+          left: null,
+          right: null,
+        },
+        right: {
+          data: 5,
+          left: null,
+          right: null,
+        },
+      });
+    });
+
+    test("deleteItem(10) does nothing if there is no existing value", () => {
+      expect(tree.deleteItem(tree.root, 10)).toEqual({
+        data: 4,
+        left: {
+          data: 1,
+          left: null,
+          right: null,
+        },
+        right: {
+          data: 5,
+          left: null,
+          right: null,
+        },
+      });
+    });
+
+    test("height(value) returns the height of the node containing the given value", () => {
+      expect(tree.height(5)).toBe(0);
+    });
+
+    test("height(3) returns undefined because the value does not exist in the tree", () => {
+      expect(tree.height(3)).toBeUndefined();
+    });
+
+    test("depth(value) returns the number of edges from the path of that node to the root node", () => {
+      expect(tree.depth(4)).toBe(0);
+    });
+
+    test("depth(value) returns the number of edges from the path of that node to the root node", () => {
+      expect(tree.depth(1)).toBe(1);
+    });
+
+    test("depth(value) returns undefined if the value is not found in the tree", () => {
+      expect(tree.depth(10)).toBeUndefined();
+    });
+
+    test("isBalanced() returns true or false if the height difference between left and right subtree is less than or greater than 1", () => {
+      expect(tree.isBalanced()).toBeTruthy();
+    });
+
+    test("insert(10) adds a new node to the right subtree", () => {
+      expect(tree.insert(10)).toEqual({
+        data: 4,
+        left: {
+          data: 1,
+          left: null,
+          right: null,
+        },
+        right: {
+          data: 5,
+          left: null,
+          right: {
+            data: 10,
+            left: null,
+            right: null,
+          },
+        },
+      });
+    });
+
+    test("insert(15) adds a new node to the right subtree", () => {
+      expect(tree.insert(15)).toEqual({
+        data: 4,
+        left: {
+          data: 1,
+          left: null,
+          right: null,
+        },
+        right: {
+          data: 5,
+          left: null,
+          right: {
+            data: 10,
+            left: null,
+            right: {
+              data: 15,
+              left: null,
+              right: null,
+            },
+          },
+        },
+      });
+    });
+
+    test("isBalanced() returns false after two new nodes were inserted to the tree", () => {
+      expect(tree.isBalanced()).toBe(false);
+    });
+
+    test("isBalanced() returns true after the tree has been rebalanced with rebalance()", () => {
+      tree.rebalance();
+
+      expect(tree.isBalanced()).toBe(true);
     });
   });
 });
